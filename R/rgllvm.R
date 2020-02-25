2eba804a7d689045b3923ed74f68b3b8aeb0dc4d
Tanya Garcia <37775239+tpgarcia@users.noreply.github.com>
2020-02-24 18:02:07 -0600

    Edited rgllvm.R file

--- R/rgllvm.R
+++ R/rgllvm.R
@@ -1,3 +1,18 @@
 #' rgllvm package
 #'
-#' This package
+#' This package provides functions to estimate parameters in a
+#' generalized linear latent variable model where the latent variable
+#' is a random intercept or random slope, and the distribution
+#' of the latent variable is left unknown
+#'
+#' @references
+#' Wei, Y., Ma, Y., Garcia, T.P. and Sinha, S. (2019). Consistent estimator
+#' for logistic mixed effect models. The Canadian Journal of Statistics, 47, 140-156. doi:10.1002/cjs.11482.
+#'
+#' Garcia, T.P. and Ma, Y. (2015). Optimal estimator for
+#' logistic model with distribution-free random intercept.
+#' Scandinavian Journal of Statistics, 43, 156-171.
+#'
+#' @docType package
+#' @name rgllvm
+NULL
